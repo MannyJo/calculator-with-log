bca54b6307fe8836438e6b2fb873ff7b2132815f
Manny Jo <jomansang@gmail.com>
2020-08-04 10:33:28 -0500

    Add mathjs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,29 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import { evaluate } from 'mathjs';
 import Keypad from '../Keypad/Keypad';
 import CalcScreen from '../CalcScreen/CalcScreen';
 import './App.css';
 
+let socket;
+
 function App() {
-  const [ result, setResult ] = useState('');
+  const [ result, setResult ] = useState('0');
+
+  useEffect(() => {
+    initSocket();
+  });
+
+  const initSocket = () => {
+    if(socket === null || socket === undefined) {
+      socket = io();
+    }
+  }
 
   const btnClick = button => () => {
     if(button === 'C') {
-      setResult('');
+      setResult('0');
     } else if(button === '=') {
       let expression;
       let value;
 
       try {
-        value = eval(result);
+        value = evaluate(result);
         expression = result + ' = ' + value;
         setResult(value);
-      } catch (error) {
-        console.error(error);
-      }
 
-      console.log(expression);
+        initSocket();
+        socket.emit('expression', expression);
+      } catch (error) {
+        alert('Wrong expression.');
+      }
     } else {
-      setResult('' + result + button);
+      if(result === '0') {
+        setResult('' + button);
+      } else {
+        setResult('' + result + button);
+      }
     }
   }
 
