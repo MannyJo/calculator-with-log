99a4b1e85b8e11cd82717cbfdae97b8a4e69468f
Manny Jo <jomansang@gmail.com>
2020-08-04 19:31:08 -0500

    Add css on keypad

--- src/components/Keypad/Keypad.js
+++ src/components/Keypad/Keypad.js
@@ -1,32 +1,32 @@
 import React from 'react';
+import './Keypad.css';
 
 const Keypad = ({ btnClick }) => {
     return (
         <div className="keypad">
-            <button onClick={btnClick('C')}>C</button>
-            <button onClick={btnClick('(')}>(</button>
-            <button onClick={btnClick(')')}>)</button>
-            <button onClick={btnClick('/')}>/</button>
+            <button onClick={btnClick('C')} className="keyColorWhite">C</button>
+            <button onClick={btnClick('(')} className="keyColorWhite">(</button>
+            <button onClick={btnClick(')')} className="keyColorWhite">)</button>
+            <button onClick={btnClick('/')} className="keyColorYellow">/</button>
 
-            <button onClick={btnClick('7')}>7</button>
-            <button onClick={btnClick('8')}>8</button>
-            <button onClick={btnClick('9')}>9</button>
-            <button onClick={btnClick('*')}>*</button>
+            <button onClick={btnClick('7')} className="keyColorDark keyFontWhite">7</button>
+            <button onClick={btnClick('8')} className="keyColorDark keyFontWhite">8</button>
+            <button onClick={btnClick('9')} className="keyColorDark keyFontWhite">9</button>
+            <button onClick={btnClick('*')} className="keyColorYellow">*</button>
 
-            <button onClick={btnClick('4')}>4</button>
-            <button onClick={btnClick('5')}>5</button>
-            <button onClick={btnClick('6')}>6</button>
-            <button onClick={btnClick('-')}>-</button>
+            <button onClick={btnClick('4')} className="keyColorDark keyFontWhite">4</button>
+            <button onClick={btnClick('5')} className="keyColorDark keyFontWhite">5</button>
+            <button onClick={btnClick('6')} className="keyColorDark keyFontWhite">6</button>
+            <button onClick={btnClick('-')} className="keyColorYellow">-</button>
 
-            <button onClick={btnClick('1')}>1</button>
-            <button onClick={btnClick('2')}>2</button>
-            <button onClick={btnClick('3')}>3</button>
-            <button onClick={btnClick('+')}>+</button>
+            <button onClick={btnClick('1')} className="keyColorDark keyFontWhite">1</button>
+            <button onClick={btnClick('2')} className="keyColorDark keyFontWhite">2</button>
+            <button onClick={btnClick('3')} className="keyColorDark keyFontWhite">3</button>
+            <button onClick={btnClick('+')} className="keyColorYellow">+</button>
 
-            <button></button>
-            <button onClick={btnClick('0')}>0</button>
-            <button onClick={btnClick('.')}>.</button>
-            <button onClick={btnClick('=')}>=</button>
+            <button onClick={btnClick('0')} className="keyColorDark keyFontWhite col-2">0</button>
+            <button onClick={btnClick('.')} className="keyColorDark keyFontWhite">.</button>
+            <button onClick={btnClick('=')} className="keyColorYellow">=</button>
         </div>
     )
 }
