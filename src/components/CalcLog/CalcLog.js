5321dff25c27ba8d1f7acd1d2e236298776f5018
Manny Jo <jomansang@gmail.com>
2020-08-04 20:13:16 -0500

    Add log css

--- src/components/CalcLog/CalcLog.js
+++ src/components/CalcLog/CalcLog.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import './CalcLog.css';
 
 const CalcLog = ({ results }) => {
     return (
-        <div>
-            <ul>
-                {
-                    results.map((result, i) => (
-                        <li key={i}>{result.expression}</li>
-                    ))
-                }
-            </ul>
+        <div className="logList">
+            <div className="logTitle">Log</div>
+            {
+                results.map((result, i) => (
+                    <div className="logItem" key={i}>{result.expression}</div>
+                ))
+            }
         </div>
     );
 }
